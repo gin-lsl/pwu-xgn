191efaf4d3432111f4f6cd15f30c154cf86a75c2
gin-lsl <gin_lsl@outlook.com>
2018-05-20 00:51:55 +0800

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getParagraphText() {
+    return element(by.css('gu-root h1')).getText();
+  }
+}
